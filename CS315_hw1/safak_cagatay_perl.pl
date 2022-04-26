e9944a446608372119c30ab72c989e1592f8d8f4
Cagatay Safak <63645814+cgtysafak@users.noreply.github.com>
2022-04-26 22:13:12 +0300

    Update safak_cagatay_perl.pl
    Indentation

--- CS315_hw1/safak_cagatay_perl.pl
+++ CS315_hw1/safak_cagatay_perl.pl
@@ -1,22 +1,29 @@
 $a = 10; # global
+
 sub foo
 {
-return $a;
+    return $a;
 }
+
 sub staticScope
 {
-my $a = 99;
-return foo();
+    my $a = 99;
+    return foo();
 }
+
 print staticScope(); # a is returned as ‘10’
+
 $b = 100; # global
+
 sub bar
 {
-return $b;
+    return $b;
 }
+
 sub dynamicScope
 {
-local $b = 999;
-return bar();
+    local $b = 999;
+    return bar();
 }
+
 print dynamicScope(); # a is returned as ‘999’
