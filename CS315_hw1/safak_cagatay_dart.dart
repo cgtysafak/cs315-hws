c2d17bf9ea152630d72c586ddbb78f07a36dc5b4
Cagatay Safak <63645814+cgtysafak@users.noreply.github.com>
2022-04-26 22:11:02 +0300

    Update safak_cagatay_dart.dart
    Indentation

--- CS315_hw1/safak_cagatay_dart.dart
+++ CS315_hw1/safak_cagatay_dart.dart
@@ -1,23 +1,30 @@
 String globalVariable = 'global\n';
+
 void func_1()
 {
-String outerVariable = 'outer\n';
-print(globalVariable);
-print(outerVariable);
-func_2()
-{
-String innerVariable = 'inner\n';
-print(globalVariable);
-print(outerVariable);
-func_3()
-{
-print(globalVariable);
-print(outerVariable);
-print(innerVariable);
+  String outerVariable = 'outer\n';
+  
+  print(globalVariable);
+  print(outerVariable);
+  
+  func_2()
+  {
+    String innerVariable = 'inner\n';
+    print(globalVariable);
+    print(outerVariable);
+    
+    func_3()
+    {
+      print(globalVariable);
+      print(outerVariable);
+      print(innerVariable);
+    }
+    
+    func_3();
+  }
+  
+  /* print(innerVariable); */ // this line gives an error.
+  func_2();
 }
-func_3();
-}
-/* print(innerVariable); */ // this line gives an error.
-func_2();
-}
+
 void main() => func_1();
